3c677ebf0038dc5676b358881766ac5553e00465
Christopher Pang <matthewstirbate@gmail.com>
2017-04-16 11:22:02 -0700

    Init Commit
    Initial push of personal site

---
+++ controller.js
@@ -0,0 +1,56 @@
+var app = angular.module('personalPage', []);
+
+app.controller('ChrisController', function($scope, $timeout) {
+  
+  $scope.homePage = true;
+  $scope.aboutMePage = false;
+  $scope.aboutMe = true;
+  $scope.workAndEducation = false;
+  $scope.projects = false;
+  $scope.contactMe = false;
+  
+  $scope.nameButtonClicked = function(){
+     $scope.homePage = false;
+    $scope.aboutMePage = true;
+     $scope.workAndEducation = false;
+    $scope.contactMe = false;
+    $scope.projects = false;
+  }
+  
+  $scope.aboutButtonClicked = function(){
+    $scope.homePage = false;
+    $scope.aboutMePage = true;
+     $scope.workAndEducation = false;
+    $scope.contactMe = false;
+    $scope.projects = false;
+  }
+  
+  $scope.workButtonClicked = function(){
+    $scope.homePage = false;
+    $scope.aboutMePage = false;
+     $scope.workAndEducation = true;
+    $scope.contactMe = false;
+    $scope.projects = false;
+  }
+  
+  $scope.projectButtonClicked = function(){
+    $scope.homePage = false;
+    $scope.aboutMePage = false;
+     $scope.workAndEducation = false;
+    $scope.contactMe = false;
+    $scope.projects = true;
+  }
+  
+  
+  
+  
+  $scope.contactButtonClicked = function(){
+    $scope.homePage = false;
+    $scope.aboutMePage = false;
+     $scope.workAndEducation = false;
+    $scope.contactMe = true;
+    $scope.projects = false;
+  }
+  
+  
+})
